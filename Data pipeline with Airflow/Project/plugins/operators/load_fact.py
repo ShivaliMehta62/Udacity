ac54fb6356d060480e1aaf36fcb41b1181fa62ea
Shivali <shivalimehta62@gmail.com>
2021-11-04 23:24:55 +0530

    Add files via upload

---
+++ Data pipeline with Airflow/Project/plugins/operators/load_fact.py
@@ -0,0 +1,30 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadFactOperator(BaseOperator):
+
+    ui_color = '#F98866'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id ="",
+                 table ="",
+                 sql_statement ="",
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.sql_statement=sql_statement
+
+    def execute(self, context):
+        """
+        Loading Fact Table.
+        """        
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info(f"Loading fact table {self.table}")
+        sql ="""INSERT INTO {}
+                 {};
+                 COMMIT;""".format(self.table,self.sql_statement)
+        redshift.run(sql)
